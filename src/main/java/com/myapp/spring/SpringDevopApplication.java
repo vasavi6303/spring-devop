76b6149914913ef9af67f94472da94e32d838e93
vasavi6303 <vasavisai333@gmail.com>
2021-02-22 12:52:46 +0530

    first commit

---
+++ src/main/java/com/myapp/spring/SpringDevopApplication.java
@@ -0,0 +1,13 @@
+package com.myapp.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDevopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDevopApplication.class, args);
+	}
+
+}
